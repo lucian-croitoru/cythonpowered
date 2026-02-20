96a140c04098aef685e005566bc7536d966e31ca
Lucian Croitoru <lucianalexandru.croitoru@gmail.com>
2026-02-20 23:35:59 +0200

    Separate listed functions by module

--- utils/definitions/list_functions.py
+++ utils/definitions/list_functions.py
@@ -1,10 +1,9 @@
-from itertools import chain
 from utils.definitions._base import BaseFunctionDefinitionPrinter
 from utils.definitions._random import RANDOM_DEFINITION_PAIRS
 from utils.definitions._dateutil import DATEUTIL_DEFINITION_PAIRS
 
 
-ALL_PAIRS = list(chain(RANDOM_DEFINITION_PAIRS, DATEUTIL_DEFINITION_PAIRS))
+ALL_PAIRS = [RANDOM_DEFINITION_PAIRS, DATEUTIL_DEFINITION_PAIRS]
 
 
 class AllFunctionDefinitionPrinter(BaseFunctionDefinitionPrinter):
